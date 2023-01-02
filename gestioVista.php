4cb7b0fcfdd82a8b2b27ef3e7a6d29689ec7fc22
Guillem <guillemmj33@gmail.com>
2023-01-02 17:08:14 +0100

    part B acabada tota

--- gestioVista.php
+++ gestioVista.php
@@ -6,16 +6,18 @@ $objDB = new DbConnect;
 $conn = $objDB->connect();
 
 /*********************** NOVA SÈRIE HTML ***********************/
-//fem comprovació de que la sèrie no existeix
 if (isset($_POST['form_submit'])) {
   $nomSerie = $_POST["nomSerie"];
   $plataformaSerie = $_POST["plataformaSerie"];
-  $temporadesPrevistes = filter_input(INPUT_POST, "temporadesPrevistes", FILTER_VALIDATE_INT);
+  $temporadesPrevistes = $_POST["temporadesPrevistes"];
+
+  echo $nomSerie, $plataformaSerie, $temporadesPrevistes;
 
   try {
+    //fem comprovació de que la sèrie no existeix
     $validation_query = $conn->prepare("SELECT * FROM serie WHERE nom = '$nomSerie'");
     $validation_query->execute([$nomSerie]);
-    if ($validation_query->rowCount() > 0) {
+    if ($validation_query->rowCount()>0) {
       echo '
       <div style="font-size:1.4rem;font-weight:bold;text-align:center;border:1px solid black;margin-left:350px;margin-right:350px;padding:50px;background-color:grey;border-radius:10px">
         La sèrie que vols afegir ja existeix. Si us plau, escull una altra.
@@ -24,10 +26,10 @@ if (isset($_POST['form_submit'])) {
       ';
     } else {
       //Introduïm les dades amb protecció contra atacs 
-      $insert = $conn->prepare("INSERT INTO serie (nom, plataforma, qualificacio, temporadesPrevistes) VALUES (:nom, :plataformaSerie, :temporadesPrevistes)");
-      $insert->bindValue( ":nom", $nomSerie);
-      $insert->bindValue( ":plataforma", $plataformaSerie);
-      $insert->bindValue( ":temporadesPrevistes", $temporadesPrevistes);
+      $insert = $conn->prepare("INSERT INTO serie (nom, plataforma, qualificacio, temporadesPrevistes) VALUES (:nomSerie, :plataformaSerie, null, :temporadesPrevistes)");
+      $insert->bindValue(":nomSerie", $nomSerie);
+      $insert->bindValue(":plataformaSerie", $plataformaSerie);
+      $insert->bindValue(":temporadesPrevistes", $temporadesPrevistes);
       if($insert->execute([$nomSerie, $plataformaSerie, $temporadesPrevistes])){
         echo '
         <div style="font-size:1.4rem;font-weight:bold;text-align:center;border:1px solid black;margin-left:350px;margin-right:350px;padding:50px;background-color:green;border-radius:10px">
